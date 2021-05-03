b1c3cd6cebe8c298a8f65ee301211ff9a0dbef95
juliafix <mail@juliafix.de>
2021-05-03 11:10:42 +0200

    Hinzufügen neuer Items

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/DetailViewActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/DetailViewActivity.java
@@ -1,24 +1,52 @@
 package org.dieschnittstelle.mobile.android.skeleton;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class DetailViewActivity extends AppCompatActivity {
 
     public static final String ARG_ITEM = "item";
     private EditText itemNameText;
     private String item;
+    private FloatingActionButton saveButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailview);
-        itemNameText= findViewById(R.id.itemName);
 
+        // 1. Bedien- und Anteigeelemente auslesen
+        itemNameText = findViewById(R.id.itemName);
+        saveButton = findViewById(R.id.saveButton);
+
+        // 2. Bedienelemente bedienbar machen
+        saveButton.setOnClickListener(v -> this.onSaveItem());
+
+        // 3. Ansicht mit Daten füllen
         item = getIntent().getStringExtra(ARG_ITEM);
-        itemNameText.setText(item);
+
+        if (item != null) {
+            itemNameText.setText(item);
+        }
+    }
+
+    protected void onSaveItem() {
+        String itemName = this.itemNameText.getText().toString();
+
+        // 4. Wieder an Main zurückgeben
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra(ARG_ITEM, itemName);
+
+        this.setResult(Activity.RESULT_OK, returnIntent);
+
+        finish();
+
     }
 }
--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/MainActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/MainActivity.java
@@ -1,36 +1,44 @@
 package org.dieschnittstelle.mobile.android.skeleton;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewGroup listView;
+    private FloatingActionButton addNewItemButton;
+    private static final int CALL_DETAILVIEW_FOR_CREATE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.listView = findViewById(R.id.listView);
+        this.addNewItemButton = findViewById(R.id.addNewItemButton);
 
         for (int i = 0; i < this.listView.getChildCount(); i++) {
             TextView currentChild = (TextView) this.listView.getChildAt(i);
             currentChild.setOnClickListener(v -> {
-                //onItemSelected(currentChild.getText().toString());
-                showFeedbackMessage(
-                        String.format(
-                                getResources()
-                                        .getString(R.string.simple_message_template), currentChild.getText()));
+                onItemSelected(currentChild.getText().toString());
+                //showFeedbackMessage(
+                //        String.format(
+                //                getResources()
+                //                        .getString(R.string.simple_message_template), currentChild.getText()));
             });
         }
 
+        this.addNewItemButton.setOnClickListener(v -> this.onItemCreationRequested());
     }
 
     protected void onItemSelected(String itemName) {
@@ -40,7 +48,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    protected void onItemCreationRequested() {
+        Intent detailViewForCreate = new Intent(this, DetailViewActivity.class);
+        this.startActivityForResult(detailViewForCreate, CALL_DETAILVIEW_FOR_CREATE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CALL_DETAILVIEW_FOR_CREATE) {
+            if (resultCode == Activity.RESULT_OK) {
+                this.onNewItemCreated(data.getStringExtra(DetailViewActivity.ARG_ITEM));
+            } else {
+                showFeedbackMessage("Rückgabe von DetailView: " + requestCode);
+            }
+
+        }
+    }
+
     protected void showFeedbackMessage(String msg) {
         Snackbar.make(findViewById(R.id.rootView), msg, Snackbar.LENGTH_INDEFINITE).show();
+    }
+
+    protected void onNewItemCreated(String itemName) {
+        TextView newItemView = (TextView) getLayoutInflater().inflate(R.layout.activity_main_listitem, null);
+        newItemView.setText(itemName);
+        this.listView.addView(newItemView);
     }
 }
